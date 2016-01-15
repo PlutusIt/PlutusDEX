ada7854d3996bf218f1b8b8dba7639d912652a9d
Gerbrand van Dieijen <gerbrand@vandieijen.nl>
2016-01-15 14:24:42 +0100

    Replaced with example from https://github.com/ethereum/dapp-bin/blob/master/standardized_contract_apis/currency.sol 8f6d000  on 6 Sep 2015

--- plutons.sol
+++ plutons.sol
@@ -1,19 +1,68 @@
-contract pluton { 
-    mapping (address => uint) public coinBalanceOf;
-    event CoinTransfer(address sender, address receiver, uint amount);
+//Subcurrency, based on https://github.com/ethereum/dapp-bin/blob/master/standardized_contract_apis/currency.sol, 8f6d000  on 6 Sep 201
+contract plutons {
 
-  /* Initializes contract with initial supply tokens to the creator of the contract */
-  function pluton(uint supply) {
-        if (supply == 0) supply = 13000000; //TODO Might want to multiply depending if we want fractional pluton
-        coinBalanceOf[msg.sender] = supply;
+    struct Account {
+        uint balance;
+        mapping ( address => uint) withdrawers;
     }
 
-  /* Very simple trade function */
-    function sendCoin(address receiver, uint amount) returns(bool sufficient) {
-        if (coinBalanceOf[msg.sender] < amount) return false;
-        coinBalanceOf[msg.sender] -= amount;
-        coinBalanceOf[receiver] += amount;
-        CoinTransfer(msg.sender, receiver, amount);
-        return true;
+    mapping ( address => Account ) accounts;
+
+    event CoinSent(address indexed from, uint256 value, address indexed to);
+
+    function currency() {
+        //13 million plutons
+        accounts[msg.sender].balance = 13000000;
+    }
+
+    function sendCoin(uint _value, address _to) returns (bool _success) {
+        if (accounts[msg.sender].balance >= _value && _value < 340282366920938463463374607431768211456) {
+            accounts[msg.sender].balance -= _value;
+            accounts[_to].balance += _value;
+            CoinSent(msg.sender, _value, _to);
+            _success = true;
+        }
+        else _success = false;
+    }
+
+    function sendCoinFrom(address _from, uint _value, address _to) returns (bool _success) {
+        uint auth = accounts[_from].withdrawers[msg.sender];
+        if (accounts[_from].balance >= _value && auth >= _value && _value < 340282366920938463463374607431768211456) {
+            accounts[_from].withdrawers[msg.sender] -= _value;
+            accounts[_from].balance -= _value;
+            accounts[_to].balance += _value;
+            CoinSent(_from, _value, _to);
+            _success = true;
+            _success = true;
+        }
+        else _success = false;
+    }
+
+    function coinBalance() constant returns (uint _r) {
+        _r = accounts[msg.sender].balance;
+    }
+
+    function coinBalanceOf(address _addr) constant returns (uint _r) {
+        _r = accounts[_addr].balance;
+    }
+
+    function approve(address _addr) {
+        accounts[msg.sender].withdrawers[_addr] = 340282366920938463463374607431768211456;
+    }
+
+    function isApproved(address _proxy) returns (bool _r) {
+        _r = (accounts[msg.sender].withdrawers[_proxy] > 0);
+    }
+
+    function approveOnce(address _addr, uint256 _maxValue) {
+        accounts[msg.sender].withdrawers[_addr] += _maxValue;
+    }
+
+    function isApprovedOnceFor(address _target, address _proxy) returns (uint256 _r) {
+        _r = accounts[_target].withdrawers[_proxy];
+    }
+
+    function disapprove(address _addr) {
+        accounts[msg.sender].withdrawers[_addr] = 0;
     }
 }
