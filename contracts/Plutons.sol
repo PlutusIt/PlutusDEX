8318cdb2278102efe42ce0c5778a10602e896aca
GregoryBetz <greg.betz@consensys.net>
2016-06-20 18:36:03 -0400

    update constructor parameters

--- contracts/Plutons.sol
+++ contracts/Plutons.sol
@@ -1,5 +1,6 @@
 
 import "HumanStandardToken.sol";
 
-contract Plutons is HumanStandardToken(20000000, "Pluton", 8, "PLU") {
+// Creates 20,000,000.00000000 Pluton (PLU) Tokens
+contract Plutons is HumanStandardToken(2000000000000000, "Pluton", 8, "PLU") {
 }
