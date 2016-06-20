45ea9a463dd81c6ffab151a41fbd6c41c8873529
GregoryBetz <greg.betz@consensys.net>
2016-06-20 19:07:44 -0400

    small change

--- contracts/Pluton.sol
+++ contracts/Pluton.sol
@@ -2,5 +2,5 @@
 import "HumanStandardToken.sol";
 
 // Creates 20,000,000.00000000 Pluton (PLU) Tokens
-contract Plutons is HumanStandardToken(2000000000000000, "Pluton", 8, "PLU") {
+contract Pluton is HumanStandardToken(2000000000000000, "Pluton", 8, "PLU") {
 }
