4f595d0ccbe87f31e1e8d5995271d4eaf6bbe3d3
Gerbrand van Dieijen <gerbrand@vandieijen.nl>
2016-09-02 14:03:58 +0200

    Comment improved

--- contracts/Pluton.sol
+++ contracts/Pluton.sol
@@ -1,16 +1,9 @@
 /*
-This Token Contract implements the standard token functionality (https://github.com/ethereum/EIPs/issues/20) as well as the following OPTIONAL extras intended for use by humans.
+The Pluton Contract implements the standard token functionality (https://github.com/ethereum/EIPs/issues/20) as well as the following OPTIONAL extras intended for use by humans.
 
-In other words. This is intended for deployment in something like a Token Factory or Mist wallet, and then used by humans.
-Imagine coins, currencies, shares, voting weight, etc.
-Machine-based, rapid creation of many tokens would not necessarily need these extra features or will be minted in other manners.
-
-1) Initial Finite Supply (upon creation one specifies how much is minted).
-2) In the absence of a token registry: Optional Decimal, Symbol & Name.
-3) Optional approveAndCall() functionality to notify a contract if an approval() has occurred.
+Pluton contract extends HumanStandardToken, https://github.com/consensys/tokens
 
 .*/
-
 contract Token {
 
     /// @return total amount of tokens
